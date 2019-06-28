ca3889410a624b39ad5e2f9c2dcdd86928638844
mjdufort <matt.dufort@gmail.com>
2019-06-28 16:32:58 -0700

    Update documentation for getModelResults.MAST

--- R/getModelResults.MAST.r
+++ R/getModelResults.MAST.r
@@ -5,7 +5,7 @@
 #' @param zlm_result The zlm fit results, generally the output of \code{MAST::zlm.SingleCellAssay}
 #' @param lrTest_result The likelihood-ratio test results, generally the output of \code{MAST::lrTest}. Needed for determinations of significance.
 #' @param contrast character, the name of the contrast for which to determine significance (and possibly logFC).
-#' @param contrast.type character, the type of contrast being extracted. Must be specified, as logFC can only be extracted for binary and/or continuous contrasts. Fro categorical contrasts, only significance values are extracted.
+#' @param contrast.type character, the type of contrast being extracted. Must be specified, as logFC can only be extracted for binary and/or continuous contrasts. For categorical contrasts, only significance values are extracted. Options are "continuous", "binary", "categorical"; partial matches allowed.
 #' @param method.p_adjust character, the method to be used for calculating adjusted p-values. Passed to \code{p.adjust}. Defaults to "BH".
 #' @export
 #' @return A data frame with one row per gene, and columns for the gene name, (optionally logFC,) p-value, and adjusted p-values.
